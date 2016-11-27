2008c9826bcede33ef4cb845b4298a6ada3a08bb
cho <you-smile@nate.com>
2016-11-28 05:43:03 +0900

    2

--- app/src/main/java/com/bit2016/linearlayoutexample/SampleLayoutCodeActivity.java
+++ app/src/main/java/com/bit2016/linearlayoutexample/SampleLayoutCodeActivity.java
@@ -1,8 +1,33 @@
 package com.bit2016.linearlayoutexample;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 /**
- * Created by cho on 2016-11-28.
+ * Created by bit-user on 2016-11-25.
  */
 
-public class SampleLayoutCodeActivity {
+public class SampleLayoutCodeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Hello");
+
+        Button btn = new Button(this);
+        btn.setText("button");
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        linearLayout.addView(tv);
+        linearLayout.addView(btn);
+
+        setContentView(linearLayout);
+    }
 }
