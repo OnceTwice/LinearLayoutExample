f8025cba150244c51c5c2cadd0a2b22e7127713e
cho <you-smile@nate.com>
2016-11-28 05:35:03 +0900

    1

--- app/src/main/java/com/bit2016/linearlayoutexample/MainActivity.java
+++ app/src/main/java/com/bit2016/linearlayoutexample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bit2016.linearlayoutexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        setContentView(R.layout.normal);
+    }
+
+    public void onButton2Clicked(View v) {
+        setContentView(R.layout.padding);
+    }
+
+    public void onButton3Clicked(View v) {
+       setContentView(R.layout.gravity);
+    }
+
+    public void onButton4Clicked(View v) {
+        setContentView(R.layout.weight);
+    }
+
+    public void onButton5Clicked(View v) {
+        setContentView(R.layout.baseline);
+    }
+
+    public void onButton6Clicked(View v) {
+        setContentView(R.layout.gravitytext01);
+    }
+
+    public void onButton7Clicked(View v) {
+        setContentView(R.layout.gravitytext02);
+    }
+
+    public void onButton8Clicked(View v) {
+        setContentView(R.layout.gravitytext03);
+    }
+
+    public void onButton9Clicked(View v) {
+        Intent intent = new Intent(getApplicationContext(), SampleLayoutCodeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/bit2016/linearlayoutexample/SampleLayoutCodeActivity.java
@@ -0,0 +1,8 @@
+package com.bit2016.linearlayoutexample;
+
+/**
+ * Created by cho on 2016-11-28.
+ */
+
+public class SampleLayoutCodeActivity {
+}
